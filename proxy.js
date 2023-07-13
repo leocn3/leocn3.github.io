2277d57dbc86f19ec91091b9164dc3550160d59a
leocn3 <97005870+leocn3@users.noreply.github.com>
2023-07-13 20:10:08 +0800

    Add files via upload

---
+++ proxy.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fetch = require('node-fetch');
+
+const server = http.createServer(async (req, res) => {
+  const proxyUrl = 'http://120.26.221.156/API/HostCD/latest'; // 目标API的地址
+  const url = `${proxyUrl}${req.url}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  res.writeHead(200, {
+    'Access-Control-Allow-Origin': '*', // 设置允许的域，可以是具体的域名或通配符*
+    'Content-Type': 'application/json',
+  });
+  res.end(JSON.stringify(data));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Proxy server listening on port ${PORT}`);
+});
