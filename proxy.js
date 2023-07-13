7d133b59595035d1caf63d7b0900024f80328aac
leocn3 <97005870+leocn3@users.noreply.github.com>
2023-07-13 20:53:01 +0800

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -15,7 +15,7 @@ const server = http.createServer(async (req, res) => {
   res.end(JSON.stringify(data));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Proxy server listening on port ${PORT}`);
 });
